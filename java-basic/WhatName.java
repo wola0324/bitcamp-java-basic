59b8ae504e291d623d23d75ab80f0761c5374d03
wola0324 <wola0324@gmail.com>
2019-08-09 18:12:38 +0900

    190809 1811

--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 class WhatName{
     public static void main(String[] args){
-        System.out.print("????????");
+        System.out.println("이름이 뭐니 ?");
+        String name = "";
+        // 값을 입력받는 지점
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.println("이름은 " + name + "이다");
+        System.out.print("몇살이에요?");
+        int age = 0;
+        age = scanner.nextInt();
+        System.out.print("나이는 "+ age +" 입니다.");
     }
 }
